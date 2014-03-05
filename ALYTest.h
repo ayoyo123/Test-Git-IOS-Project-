ca15c8af9b9fdb721807d6c3e92b545bce98b427
Alex Yeo <leey@Alexs-MacBook-Pro-2.local>
2014-03-04 23:37:22 -0500

    added a test class

---
+++ ALYTest.h
@@ -0,0 +1,13 @@
+//
+//  ALYTest.h
+//  Git Test
+//
+//  Created by Alex Yeo  on 3/4/14.
+//  Copyright (c) 2014 Alex Yeo . All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ALYTest : NSObject
+
+@end
---
+++ ALYTest.m
@@ -0,0 +1,13 @@
+//
+//  ALYTest.m
+//  Git Test
+//
+//  Created by Alex Yeo  on 3/4/14.
+//  Copyright (c) 2014 Alex Yeo . All rights reserved.
+//
+
+#import "ALYTest.h"
+
+@implementation ALYTest
+
+@end
